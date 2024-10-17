22de561cdf4aaa2406cc9ca7468508aa22945c62
S-JOBI23-058\student <231064@st.yoshida-g.ac.jp>
2024-10-17 16:00:01 +0900

    API作成のためスクリプト作成

---
+++ Assets/Scripts/CreateStageManager.cs
@@ -0,0 +1,26 @@
+//=================================================
+//クリエイトステージ
+//三宅歩人
+//2024/10/16
+//=================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateStageManager : MonoBehaviour
+{
+    //自機の座標[type][0(x) or 1(y)]
+    float[][] postions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
